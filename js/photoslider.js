1b6196dab96606edfc9f7f8415c392d41e98098a
vincent <ikbengerrit92@gmail.com>
2020-12-09 13:37:37 +0100

    :bug: PHOTOSLIDER BUGS FIXED AND AUTOMATIC SLIDER FUNCTION ADDED

--- js/photoslider.js
+++ js/photoslider.js
@@ -1,28 +1,34 @@
 let slideIndex = 1;
+let myTimer;
+let timeout = 5000;
 showSlides(slideIndex);
 
+window.addEventListener("load",function() {
+  showSlides(slideIndex);
+  myTimer = setInterval(function(){plusSlides(1)}, timeout);
+});
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, timeout);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, timeout);
+  }
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function showSlides(n){
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
 }
